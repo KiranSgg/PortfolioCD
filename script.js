d3587ffcf6724a9d8f1a9c8bdd6b9f4ae65ec782
Kiran Shrestha <83537517+KiranSgg@users.noreply.github.com>
2022-05-01 15:00:35 +0545

    timer increase

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "5 may 2022";
+const newYears = "2 july 2022";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
