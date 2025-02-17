43eb0bc46abcd6a2dd7d07a7e9c1d3b42167c361
Kiran Shrestha <83537517+KiranSgg@users.noreply.github.com>
2025-02-17 23:31:32 +0545

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "1 august 2023";
+const newYears = "17 march 2025";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
